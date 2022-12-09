03f67af4b80972a764b74a6391ef32cc1a0a7673
solokool66 <solokool66@gmail.com>
2022-12-08 17:49:12 -0800

    first

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,8 +8,8 @@ int main(void)
 {
 printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(char));
 printf("size of an int: %lu byte(s)\n", (unsigned long) sizeof(int));
-printf("size of an long int: %lu byte(s)\n", (unsigned long) sizeof(long int));
-printf("size of an long long int: %lu byte(s)\n", (unsigned long) sizeof(long long int));
-printf("size of an float: %lu byte(s)\n", (unsigned long) sizeof(float));
+printf("size of a long int: %lu byte(s)\n", (unsigned long) sizeof(long int));
+printf("size of a long long int: %lu byte(s)\n", (unsigned long) sizeof(long long int));
+printf("size of a float: %lu byte(s)\n", (unsigned long) sizeof(float));
 return (0);
 }
