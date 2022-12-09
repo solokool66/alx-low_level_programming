93e396cb0426818154264ceab666f975da7095b7
solokool66 <solokool66@gmail.com>
2022-12-08 17:35:08 -0800

    list

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 /**
- * main = Prints whats typed
- * Return: Always 0
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
-printf("Size of a char: %i byte(s)\n", (unsigned long) sizeof(char));
-printf("size of an int: %i byte(s)\n", (unsigned long) sizeof(int));
-printf("size of an long int: %i byte(s)\n", (unsigned long) sizeof(long int));
-printf("size of an long long int: %i byte(s)\n", (unsigned long) sizeof(long long int));
-printf("size of an float: %i byte(s)\n", (unsigned long) sizeof(float));
+printf("Size of a char: %i byte(s)\n", sizeof(char));
+printf("size of an int: %i byte(s)\n", sizeof(int));
+printf("size of an long int: %i byte(s)\n", sizeof(long int));
+printf("size of an long long int: %i byte(s)\n", sizeof(long long int));
+printf("size of an float: %i byte(s)\n", sizeof(float));
 return (0);
 }
