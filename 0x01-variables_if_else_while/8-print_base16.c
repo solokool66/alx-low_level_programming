0642ea7b99499871bb5699df1c075bcab3d5bd3f
solokool66 <solokool66@gmail.com>
2022-12-12 15:52:55 -0800

    cool commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - Prints the number in base 16
+*Return: Always 0
+*/
+int main(void)
+{
+int num;
+char ab;
+for (num = 0; num < 10; num++)
+putchar((num % 10) + '0');
+for (ab = 'a'; ab <= 'f'; ab++)
+putchar('\n');
+return (0);
+}
