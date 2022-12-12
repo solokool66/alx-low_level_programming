7e37a8cd6bab0de2e14791e0689cc26e10694947
solokool66 <solokool66@gmail.com>
2022-12-12 15:33:18 -0800

    cool one

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - print the alphabet in reverse
+*Return: Always 0
+*/
+int main(void)
+{
+char ab;
+for (ab = 'z'; ab >= 'a'; ab--)
+{
+putchar(ab);
+putchar('\n');
+}
+return (0);
+}
