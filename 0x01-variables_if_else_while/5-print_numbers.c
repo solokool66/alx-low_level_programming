1f4a6009a78ebc683761da283d88f170b5c7c4d9
solokool66 <solokool66@gmail.com>
2022-12-12 15:15:51 -0800

    cool

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - Prints all string from 0 to 9
+*Return: always 0
+*/
+int main(void)
+{
+int num;
+for (num = 0; num < 10; num++)
+{
+printf("%d", num);
+}
+printf("\n");
+return (0);
+}
