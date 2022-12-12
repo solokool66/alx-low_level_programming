e1bc61f27e90cbe7ee1332c3e3c95f7efe008651
solokool66 <solokool66@gmail.com>
2022-12-12 10:18:55 -0800

    if else

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main- Entry Point
+*
+*return: 0 Success
+*/
+char main(void)
+{
+char = come;
+for (come = 'a'; ccome <= 'z'; come++)
+{
+putchar("%c\n", come);
+}
+return (0);
+}
