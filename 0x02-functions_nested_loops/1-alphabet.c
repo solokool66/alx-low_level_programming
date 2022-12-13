3f64bc57f05c0624ac0d8841a0c261e4b0e0c2e1
solokool66 <solokool66@gmail.com>
2022-12-13 15:54:26 -0800

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 /**
 *print_alphabet - Prints the alphabet in lower case followed by a new line
 */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 char letters;
 for (letters = 'a'; letters <= 'z'; letters++)
