2ddb17343775be993dc5a0fd506bcc8a3125ef48
solokool66 <solokool66@gmail.com>
2022-12-15 08:44:46 -0800

    our commit

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
-* convert_day - converts day of month to day of year, without accounting for leap year
+* convert_day - converts day of month to day of year,
+*		without accounting for leap year
 * @month: month in number format
 * @day: day of month
 *
