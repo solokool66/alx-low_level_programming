0383dc20c3d33be2b38e440352dc0486110c1542
solokool66 <solokool66@gmail.com>
2022-12-15 14:10:48 -0800

    my commit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,12 +1,11 @@
-#define MAIN_H
 #ifndef MAIN_H
-
-int _putchar(char c);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
+#define MAIN_H
+/**
+*main - header file for protoypes
+*void protoypes (void)
+*int protoypes (int)
+*/
 void print_numbers(void);
-void print_most_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
@@ -14,5 +13,10 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+int _putchar(char);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
 
-#endif /*MAIN_H*/
+#endif /* MAIN_H */
+
