5d8a7a2e48a028d758fa1f89e45689698a34f699
solokool66 <solokool66@gmail.com>
2022-12-15 15:16:25 -0800

    my commit

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
 *print_square - prints squares
 *@size: parameter
@@ -7,21 +8,20 @@
 
 void print_square(int size)
 {
-	int inc1, inc2;
-	if (size > 0)
-	{
-		for (inc1 = 0; inc1 < size; inc1++)
-		{
-			for (inc2 = 0; inc2 < (size - 1); inc2++)
-				_putchar('#');
-
-			_putchar('#');
-			_putchar('\n');
-		}
+int inc1, inc2;
+if (size > 0)
+{
+for (inc1 = 0; inc1 < size; inc1++)
+{
+for (inc2 = 0; inc2 < (size - 1); inc2++)
+_putchar('#');
+_putchar('#');
+_putchar('\n');
+	}
 	}
 	else
 	{
+
 		_putchar('\n');
 	}
 }
-
