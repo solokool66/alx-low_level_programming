170c436ed611ca23dda71fe2bf79f47bc4e1c101
solokool66 <solokool66@gmail.com>
2022-12-24 05:23:14 -0800

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
-
-/**
+/*
 * main - Checks the code *(&n + 5) = 98
-* Return: Always 0
 */
-
 int main(void)
 {
 int n;
 int a[5];
 int *p;
-
 a[2] = 1024;
-p = &n;
+p = &n
 /*
 * write your line of code here...
 * Remember:
@@ -22,7 +18,6 @@ p = &n;
 * - you are not allowed to code anything else than this line of code
 */
 *(&n + 5) = 98;
-/* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
 }
