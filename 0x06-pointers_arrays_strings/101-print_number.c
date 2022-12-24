701821f3715a471f6b52ed580a2379ab11a4a215
solokool66 <solokool66@gmail.com>
2022-12-23 18:15:20 -0800

    cool

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -11,18 +11,13 @@
 
 void print_number(int n)
 {
-unsigned int num;
-/*check if number is negative*/
-num = n;
+unsigned int num = n;
 if (n < 0)
 {
-_putchar(45);
-num = -n;
+_putchar('-');
+num = -num;
 }
-/* print number by recursion*/
-if (num / 10)
-{
+if ((num / 10) > 0)
 print_number(num / 10);
-}
-putchar((num % 10) + '0');
+_putchar((num % 10) + '0');
 }
