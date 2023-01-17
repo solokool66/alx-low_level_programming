42e239ef5c78aebab7b0d49ddffa47e23ad904f3
solokool66 <solokool66@gmail.com>
2023-01-17 05:00:10 -0800

    malloc task continued

---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,2 @@
+This is the README file for the task titled More malloc, free.
+We are going to look deeply into the topic called malloc and use them to allocate memories.
---
+++ 0x0C-more_malloc_free/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+  *_putchar - writes the character c to the stdout
+  *@c: The character to print out
+  *Return: On success 1
+  *on error, -1 is returned
+  */
+
+int _putchar(char c)
+{
+return (write(1, &c, 1))
+}
