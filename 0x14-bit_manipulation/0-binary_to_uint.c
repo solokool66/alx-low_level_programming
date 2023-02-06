2398ceb0073ede7f619bddefc18a8b2926ea50b0
solokool66 <solokool66@gmail.com>
2023-02-06 14:41:07 -0800

    updaste

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,13 +9,10 @@ unsigned int binary_to_uint(const char *b)
 {
 unsigned int decimal = 0;
 int str_len = 0, base = 1;
-
 if (!check_valid_string(b))
 return (0);
-
 while (b[str_len] != '\0')
 str_len++;
-
 while (str_len)
 {
 decimal += ((b[str_len - 1] - '0') * base);
@@ -24,7 +21,6 @@ str_len--;
 }
 return (decimal);
 }
-
 /**
  * check_valid_string - checks if a string has only 0's and 1's
  * @b: string to be checked
@@ -35,7 +31,6 @@ int check_valid_string(const char *b)
 {
 if (b == NULL)
 return (0);
-
 while (*b)
 {
 if (*b != '1' && *b != '0')
