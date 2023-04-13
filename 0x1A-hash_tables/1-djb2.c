0be46f098e68b718bbf4fe71bd4073eaaa9c377b
solokool66 <solokool66@gmail.com>
2023-04-13 13:03:44 -0700

    task 1 completed

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -13,7 +13,6 @@ unsigned long int hash_djb2(const unsigned char *str)
 
 	hash = 5381;
 	while ((c = *str++))
-		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-
-	return (hash);
+	hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+return (hash);
 }
