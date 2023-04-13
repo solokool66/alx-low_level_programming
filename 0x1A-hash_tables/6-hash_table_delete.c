7938257dd0312bed2df43215e5c70b700933aefb
solokool66 <solokool66@gmail.com>
2023-04-13 13:30:35 -0700

    Final correction

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -8,24 +8,23 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	unsigned long int i;
-	hash_node_t *tmp1;
-	hash_node_t *tmp2;
+unsigned long int i;
+hash_node_t *tmp1;
+hash_node_t *tmp2;
+if (ht == NULL)
+return;
 
-	if (ht == NULL)
-		return;
-
-	for (i = 0; i < ht->size; i++)
-	{
-		tmp1 = ht->array[i];
-		while ((tmp2 = tmp1) != NULL)
-		{
-			tmp1 = tmp1->next;
-			free(tmp2->key);
-			free(tmp2->value);
-			free(tmp2);
-		}
-	}
-	free(ht->array);
-	free(ht);
+for (i = 0; i < ht->size; i++)
+{
+tmp1 = ht->array[i];
+while ((tmp2 = tmp1) != NULL)
+{
+tmp1 = tmp1->next;
+free(tmp2->key);
+free(tmp2->value);
+free(tmp2);
+}
+}
+free(ht->array);
+free(ht);
 }
