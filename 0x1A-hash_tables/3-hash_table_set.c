eef47c22e39c42a624122c1620f91f8d8ced6cb6
solokool66 <solokool66@gmail.com>
2023-04-13 13:14:32 -0700

    corrected task 4

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -18,34 +18,30 @@ hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *tmp = NULL;
 
 	if (!ht || !key || !(*key) || !value)
-		return (0);
-
+	return (0);
 	index = key_index((unsigned char *)key, ht->size);
 	tmp = ht->array[index];
 
 	/* check if key exists */
 	while (tmp && strcmp(tmp->key, key) != 0)
-		tmp = tmp->next;
+	tmp = tmp->next;
 
 	/* update value if key already exists */
 	if (tmp)
 	{
-		free(tmp->value);
-		tmp->value = strdup(value);
-		return (1);
+	free(tmp->value);
+	tmp->value = strdup(value);
+	return (1);
 	}
 
 	/* add new node if key not found */
 
 	new_hash_node = malloc(sizeof(*new_hash_node));
 	if (!new_hash_node)
-		return (0);
-
-	new_hash_node->key = strdup(key);
-	new_hash_node->value = strdup(value);
-
-	new_hash_node->next = ht->array[index];
-	ht->array[index] = new_hash_node;
-
-	return (1);
+	return (0);
+new_hash_node->key = strdup(key);
+new_hash_node->value = strdup(value);
+new_hash_node->next = ht->array[index];
+ht->array[index] = new_hash_node;
+return (1);
 }
