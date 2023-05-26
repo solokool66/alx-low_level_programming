86dbe8c56d11878472e0f8ef30bcafe53a50d754
solokool66 <solokool66@gmail.com>
2023-05-25 23:44:49 -0700

    update

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,16 +1,16 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
-*free_dog - Frees dog
-*@d: dog to be freed
-*
-*/
+ * free_dog - Frees dogs.
+ * @d: The dog to be freed.
+ */
 void free_dog(dog_t *d)
 {
-if (d == NULL)
-return;
+	if (d == NULL)
+		return;
 
-free(d->name);
-free(d->owner);
-free(d);
+	free(d->owner);
+	free(d->name);
+	free(d);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,35 +1,26 @@
 #ifndef DOG_H
 #define DOG_H
 
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
-*struct dog - a new type of data
-*representing a dog
-*@name: name of dog
-*@age: age of dog
-*@owner: owner's name
-*/
+ * struct dog - Struct dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Description: This struct is for dog
+ */
 struct dog
 {
-char *name;
-float age;
-char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
-
-/**
-*dog_t - Typedef for struct dog
-*/
 typedef struct dog dog_t;
-
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-int _strlen(char *str);
-char *_strcopy(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
 #endif
