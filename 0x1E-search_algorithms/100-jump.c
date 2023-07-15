f7dc28302b840646ad95964f9f250bc25d05d4e8
solokool66 <solokool66@gmail.com>
2023-07-15 16:53:33 -0700

    100-jump updated

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -30,7 +30,7 @@ index = k * m;
 } while (index < (int)size && array[index] < value);
 printf("Value found between indexes [%d] and [%d]\n", prev, index);
 for
-(prev <= index && prev < (int)size; prev++)
+(;prev <= index && prev < (int)size; prev++)
 {
 printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
 if (array[prev] == value)
