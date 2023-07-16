66602bd0fdbb6e3b2c1116904637e0cc8a9c65e9
solokool66 <solokool66@gmail.com>
2023-07-16 10:50:38 -0700

    jump list

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -2,14 +2,15 @@
 #include <math.h>
 
 /**
- * jump_list - searches for a value in an array of
- * integers using the Jump search algorithm
- *
- * @list: input list
- * @size: size of the array
- * @value: value to search in
- * Return: index of the number
- */
+* jump_list - searches for a value in an array of
+* integers using the Jump search algorithm
+*
+* @list: input list
+* @size: size of the array
+* @value: value to search in
+* Return: index of the number
+*/
+
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
 	size_t index, k, m;
