b9292cd063c8906a94495262809090f51202ecbc
solokool66 <solokool66@gmail.com>
2023-07-16 10:53:11 -0700

    linear skip

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -1,12 +1,13 @@
 #include "search_algos.h"
 
 /**
- * linear_skip - searches for a value in a skip list
- *
- * @list: input list
- * @value: value to search in
- * Return: index of the number
- */
+* linear_skip - searches for a value in a skip list
+*
+* @list: input list
+* @value: value to search in
+* Return: index of the number
+*/
+
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
 	skiplist_t *go;
